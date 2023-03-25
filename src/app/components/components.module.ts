d65460c71a6b216180af25fda51d78669ea8e9f0
John Freddy Buitrago Quevedo <john.buitrago@insoftar.com>
2023-03-24 19:08:51 -0500

    feat: title component

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -2,9 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { TranslateModule } from '@ngx-translate/core';
+import { TitleComponent } from './title/title.component';
 
 @NgModule({
-    declarations: [],
+    declarations: [
+        TitleComponent
+    ],
+    exports: [
+        TitleComponent
+    ],
     imports: [
         CommonModule,
         FormsModule,
